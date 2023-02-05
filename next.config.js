8bd9e593754c6418dac776656f7a05c9f1c5cfa0
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2023-02-06 00:29:40 +0100

    removed remote patterns in next config

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_HOSTNAME,
-        port: "",
-        pathname: process.env.NEXT_PUBLIC_SUPABASE_PATH,
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: process.env.NEXT_PUBLIC_SUPABASE_HOSTNAME,
+  //       port: "",
+  //       pathname: process.env.NEXT_PUBLIC_SUPABASE_PATH,
+  //     },
+  //   ],
+  // },
 };
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
