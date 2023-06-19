ba7c7c9fa35b663bf20f3efb06ed259a8849d644
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2023-06-19 21:48:46 +0200

    skill icons on home displayed in grid, show tech name on hover

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
       <footer className="footer-container mx-auto px-6">
         <div className="flex h-16 flex-row items-center">
           <h1 className="text-sm font-semibold text-black">
-            &copy; 2023 - Mick Dellaert - All rights reserved
+            &copy; 2023 - Mick Dellaert
+            {/* &copy; 2023 - Mick Dellaert - All rights reserved */}
           </h1>
         </div>
       </footer>
