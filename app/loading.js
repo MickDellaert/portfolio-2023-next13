6d7a91d20fd99f5b97fbaf521bcdbe6124681037
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2023-07-05 20:04:58 +0200

    restored loading.js file

---
+++ app/loading.js
@@ -0,0 +1,10 @@
+
+function Loading() {
+  return (
+    <div className="absolute top-24 left-1/2 z-50 justify-center text-primary">
+      Loading home...
+    </div>
+  );
+}
+
+export default Loading;
