c56425b7653a5fb1053bd28fb894a4924e0018d0
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2023-08-02 20:46:36 +0200

    skills hover details mobile

--- app/home/SkillsAnimated.js
+++ app/home/SkillsAnimated.js
@@ -48,17 +48,17 @@ export const SkillsAnimated = ({ icons }) => {
     visible: { opacity: 1 },
   };
 
-  const skillIcon = notSmall
-    ? {
-        invisible: { opacity: 1 },
-        visible: { opacity: 0 },
-      }
-    : { invisible: { opacity: 1 }, visible: { opacity: 1 } };
+  // const skillIcon = notSmall
+  //   ? {
+  //       invisible: { opacity: 1 },
+  //       visible: { opacity: 0 },
+  //     }
+  //   : { invisible: { opacity: 1 }, visible: { opacity: 1 } };
 
-  // const skillIcon = {
-  //   invisible: { opacity: 1 },
-  //   visible: { opacity: 0 },
-  // };
+  const skillIcon = {
+    invisible: { opacity: 1 },
+    visible: { opacity: 0 },
+  };
 
   const skillDetails = {
     invisible: { opacity: 0 },
@@ -78,7 +78,7 @@ export const SkillsAnimated = ({ icons }) => {
 
   return (
     <>
-      <div id="skills" className="relative top-36"></div>
+      <div id="skills" className="relative top-12"></div>
 
       <div className="mx-auto mt-40">
         <motion.h2
@@ -96,34 +96,34 @@ export const SkillsAnimated = ({ icons }) => {
           initial="hidden"
           animate={controls}
           variants={container}
-          className="mx-auto flex w-[96%] flex-wrap justify-center xl:w-3/4 2xl:w-4/6"
+          className="mx-auto flex w-[96%] flex-wrap justify-center xl:w-3/4 2xl:w-[70%]"
         >
           {icons.map((skill, i) => {
             const HomeSkills = skillIcons[skill.iconCode];
             return (
-              <motion.div key={i} variants={item} className="group flex h-28 basis-1/4 flex-col py-1 md:basis-1/6">
+              <motion.div key={i} variants={item} className="group flex h-28 basis-1/4 flex-col py-1 lg:basis-1/6">
                 <motion.div
-                  className="relative h-full sm:cursor-pointer"
+                  className="relative h-full sm:cursor-pointer max-w-[184px]"
                   initial="invisible"
                   whileHover="visible"
                   variants={skillContainer}
                 >
                   <motion.div className="flex h-full flex-col justify-center" variants={skillIcon}>
-                    <HomeSkills className="m-auto h-10 w-10  fill-neutral-400 md:h-12 md:w-12" />
+                    <HomeSkills className="m-auto h-10 w-10 fill-neutral-400 md:h-12 md:w-12" />
                   </motion.div>
 
                   <motion.div
                     variants={skillDetails}
-                    className="absolute left-0 top-0 hidden h-full w-full flex-col justify-between rounded-lg bg-neutral-50 p-4 drop-shadow-xl sm:flex"
+                    className="absolute left-0 top-0 flex h-full w-full flex-col justify-between rounded-lg bg-neutral-50 p-4 drop-shadow-xl"
                   >
-                    <div className="flex h-7 items-center">
-                      <HomeSkills className="mr-3 h-7 w-7 fill-primary" />
-                      <h3 className="text-base font-bold leading-tight">{skill.iconName}</h3>
+                    <div className="flex flex-col justify-center sm:flex-row h-7 sm:items-center">
+                      <HomeSkills className="hidden sm:block mr-3 h-7 w-7 fill-primary" />
+                      <h3 className="text-sm text-center sm:text-left sm:text-base font-bold sm:leading-tight leading-tight">{skill.iconName}</h3>
                     </div>
                     <hr className="bg-neutral-50"></hr>
                     <div className="mb-1.5 flex flex-col justify-end">
-                      <div className="flex justify-between items-center">
-                        <h3 className="text-sm font-normal text-neutral-950">Skill level</h3>
+                      <div className="flex items-center justify-center sm:justify-between">
+                        <h3 className="hidden text-sm font-normal text-neutral-950 sm:block">Skill level</h3>
                         <h3 className="text-base font-bold text-neutral-950">{`${skill.level}%`}</h3>
                       </div>
                       <div className="relative w-full">
